0e9303b47b4abfc1a1cf4b27ad7333ef5d398427
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-03-13 17:07:03 -0500

    Add pydoc comment for ax.set_ylabel

--- fig_ax_scatter_ex_04.py
+++ fig_ax_scatter_ex_04.py
@@ -50,10 +50,13 @@ def main():
     ax1.plot(sample_one['y'], marker='.', markersize=8, linestyle='None')
     # add Axes title
     # https://matplotlib.org/stable/api/_as_gen/
-    #    matplotlib.axes.Axes.set_title.html
+    #     matplotlib.axes.Axes.set_title.html
     # pydoc matplotlib.axes.Axes.set_title
     ax1.set_title(label='Sample one', fontweight='bold', fontsize=12)
-    # add y axis title
+    # add y axis label
+    # https://matplotlib.org/stable/api/_as_gen/
+    #     matplotlib.axes.Axes.set_ylabel.html
+    # pydoc matplotlib.axes.Axes.set_ylabel
     ax1.set_ylabel(ylabel='y', fontweight='bold')
     # add x axis title
     ax1.set_xlabel(
@@ -77,7 +80,7 @@ def main():
     )
     # add Axes title
     # https://matplotlib.org/stable/api/_as_gen/
-    #    matplotlib.axes.Axes.set_title.html
+    #     matplotlib.axes.Axes.set_title.html
     # pydoc matplotlib.axes.Axes.set_title
     ax2.set_title(label='Sample two', fontweight='bold', fontsize=12)
     # add average line
