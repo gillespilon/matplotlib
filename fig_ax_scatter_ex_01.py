ae7531fa146c1655def9e8969944d8925724b7d9
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-03-14 10:28:10 -0500

    Add html, pydoc comment for pd.DataFrame

--- fig_ax_scatter_ex_01.py
+++ fig_ax_scatter_ex_01.py
@@ -16,6 +16,8 @@ def main():
         ]
     }
     # create DataFrames
+    # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
+    # pydoc pandas.DataFrame
     df = pd.DataFrame(data=data)
     sample_one = df[df['x'] == 1]
     # create Figure, Axes objects
