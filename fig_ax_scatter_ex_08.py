ea3a245f35a3cbbf0df6facb477888dea0c56fdf
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-03-18 18:38:25 -0500

    Create fig_ax_scatter_ex_08.py

---
+++ fig_ax_scatter_ex_08.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+"""
+Normal probability plot.
+"""
+
+import matplotlib.pyplot as plt
+
+import datasense as ds
+import pandas as pd
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
