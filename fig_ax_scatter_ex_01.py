057222803bfc0f80eecf7fdea9d8f8a6e090f542
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-03-21 13:21:36 -0500

    Tidy code

--- fig_ax_scatter_ex_01.py
+++ fig_ax_scatter_ex_01.py
@@ -9,8 +9,8 @@ import pandas as pd
 
 def main():
     data = {
-        'x': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-        'y': [
+        "x": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        "y": [
             32, 37, 35, 28, 41, 44, 35, 31, 34, 38, 42, 36, 31, 30, 31, 34,
             36, 29, 32, 31
         ]
@@ -19,7 +19,7 @@ def main():
     # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
     # pydoc pandas.DataFrame
     df = pd.DataFrame(data=data)
-    sample_one = df[df['x'] == 1]
+    sample_one = df[df["x"] == 1]
     # create Figure, Axes objects
     # https://matplotlib.org/stable/api/figure_api.html
     # class matplotlib.figure.Figure
@@ -32,7 +32,7 @@ def main():
     # create scatter plot
     # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.plot.html
     # pydoc matplotlib.axes.Axes.plot
-    ax.plot(sample_one['y'], linestyle='None', marker='.', markersize=8)
+    ax.plot(sample_one["y"], linestyle="None", marker=".", markersize=8)
     # save image as file
     # https://matplotlib.org/stable/api/
     #     figure_api.html#matplotlib.figure.Figure.savefig
@@ -40,5 +40,5 @@ def main():
     fig.savefig(fname="fig_ax_scatter_ex_01.svg", format="svg")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
