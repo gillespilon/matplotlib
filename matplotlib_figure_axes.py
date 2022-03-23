99a1abaf1cd108e6b03ad38b527d9d585a016f03
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-03-23 10:32:20 -0500

    Use built-in data, not read

--- matplotlib_figure_axes.py
+++ matplotlib_figure_axes.py
@@ -10,6 +10,8 @@ https://docs.python.org/3/library/datetime.html#module-datetime
 https://matplotlib.org/api/dates_api.html#matplotlib-date-format
 """
 
+from typing import Tuple
+
 from numpy.polynomial import polynomial as nppoly
 import matplotlib.pyplot as plt
 import datasense as ds
@@ -179,6 +181,7 @@ def main():
     ds.html_figure(file_name='single_figure_two_axes.svg')
     # Single figure, four axes
     axes_title = ['AQ1', 'AQ2', 'AQ3', 'AQ4']
+    df1, df2, df3, df4 = create_dataframe()
     fig = plt.figure(figsize=figsize)
     fig_title = "Anscombe's Quartet"
     fig.suptitle(
@@ -186,7 +189,8 @@ def main():
         fontweight='bold'
     )
     for index in range(1, 5):
-        df = ds.read_file(file_name=f'aq{index}.csv')
+        df = eval(f"df{index}")
+        # df = ds.read_file(file_name=f'aq{index}.csv')
         ax = fig.add_subplot(2, 2, index)
         ax.plot(
             df['x'],
@@ -196,7 +200,8 @@ def main():
             color=colour1
         )
         b, m = nppoly.polyfit(df['x'], df['y'], 1)
-        ax.plot(df['x'], m*df['x'] + b, '-', color=colour2)
+        equation = f"$y = {b:.1f} + {m:.1f}x$"
+        ax.plot(df['x'], m*df['x'] + b, '-', color=colour2, label=equation)
         ax.set_ylim(
             bottom=2,
             top=14
@@ -208,6 +213,7 @@ def main():
         ax.set_title(label=axes_title[index-1])
         ax.set_ylabel(ylabel=y_axis_label)
         ax.set_xlabel(xlabel=x_axis_label)
+        ax.legend(frameon=False, fontsize=8)
         ds.despine(ax=ax)
     plt.tight_layout(pad=3)
     fig.savefig(
@@ -220,6 +226,39 @@ def main():
         original_stdout=original_stdout,
         output_url=output_url
     )
+
+
+def create_dataframe() -> Tuple[pd.DataFrame]:
+    """
+    Load data into separate dataframes.
+    """
+    data_aq1 = {
+        "x": [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5],
+        "y": [
+            8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68
+        ],
+    }
+    data_aq2 = {
+        "x": [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5],
+        "y": [9.14, 8.14, 8.74, 8.77, 9.26, 8.1, 6.13, 3.1, 9.13, 7.26, 4.74],
+    }
+    data_aq3 = {
+        "x": [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5],
+        "y": [
+            7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73
+        ],
+    }
+    data_aq4 = {
+        "x": [8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8],
+        "y": [
+            6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.5, 5.56, 7.91, 6.89
+        ],
+    }
+    df1 = pd.DataFrame(data=data_aq1)
+    df2 = pd.DataFrame(data=data_aq2)
+    df3 = pd.DataFrame(data=data_aq3)
+    df4 = pd.DataFrame(data=data_aq4)
+    return (df1, df2, df3, df4)
 
 
 if __name__ == '__main__':
