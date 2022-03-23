e627a40f86f461900e6317886063085af2453586
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-03-22 21:26:30 -0500

    Add scipy

--- fig_ax_scatter_ex_08.py
+++ fig_ax_scatter_ex_08.py
@@ -5,12 +5,14 @@ Normal probability plot.
 
 import matplotlib.pyplot as plt
 
+from scipy import stats
 import datasense as ds
 import pandas as pd
 
 
 def main():
-    pass
+    df = ds.create_dataframe_norm()
+    print(type(df).__name__)
 
 
 if __name__ == '__main__':
