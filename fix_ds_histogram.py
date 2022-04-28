acbcc14af601fd2a0d740d83962acfd92e7a1432
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-04-28 15:44:08 -0500

    Use sample size to test percentages

--- fix_ds_histogram.py
+++ fix_ds_histogram.py
@@ -10,7 +10,7 @@ import datasense as ds
 
 
 def main():
-    s = ds.random_data(size=100)
+    s = ds.random_data(size=300)
     fig, ax = ds.plot_histogram(series=s, bin_label_bool=True)
     ax.set_xlabel(xlabel="x axis label", labelpad=30)
     plt.tight_layout()
