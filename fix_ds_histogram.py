b63753fd237285440a7a00c3bba817633fe36eee
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-04-29 09:08:01 -0500

    Add x-axis tick label format

--- fix_ds_histogram.py
+++ fix_ds_histogram.py
@@ -5,6 +5,7 @@ Fix issue with ds.plot_histogram for bin_label_bool=True.
 Use to enhance example in ds.plot_histogram().
 """
 
+from matplotlib.ticker import StrMethodFormatter
 import matplotlib.pyplot as plt
 import datasense as ds
 
@@ -15,6 +16,9 @@ def main():
     # fig, ax = ds.plot_histogram(series=s)
     fig, ax = ds.plot_histogram(series=s, bin_label_bool=True)
     ax.set_xlabel(xlabel="x axis label", labelpad=30)
+    ax.xaxis.set_major_formatter(
+        formatter=StrMethodFormatter(fmt="{x:0.1f}")
+    )
     ax.set_ylabel(ylabel="y axis label")
     plt.tight_layout()
     fig.savefig(fname="fix_ds_histogram.svg")
