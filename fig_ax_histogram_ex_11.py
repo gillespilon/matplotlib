2a7dce8a32c6d84b6141e9cb7deca7f0e61643ec
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-04-29 11:47:40 -0500

    Tidy code, add axes labels¸title

--- fig_ax_histogram_ex_11.py
+++ fig_ax_histogram_ex_11.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python3
 """
-Fix issue with ds.plot_histogram for bin_label_bool=True.
-
-Use to enhance example in ds.plot_histogram().
+Histogram with bin size adjustment and tick formatting.
 """
+
+from pathlib import Path
 
 from matplotlib.ticker import StrMethodFormatter
 import matplotlib.pyplot as plt
@@ -11,17 +11,22 @@ import datasense as ds
 
 
 def main():
+    path_histogram = Path("fig_ax_histogram_ex_11.svg", format="svg")
     s = ds.random_data(size=300)
+    ylabel = "Y axis label"
+    xlabel = "X axis label"
+    title = "Histogram"
     ds.style_graph()
     # fig, ax = ds.plot_histogram(series=s)
     fig, ax = ds.plot_histogram(
         series=s, bin_range=(-4, 4), number_bins=8, bin_label_bool=True
     )
-    ax.set_xlabel(xlabel="x axis label", labelpad=30)
+    ax.set_xlabel(xlabel=xlabel, labelpad=30)
     ax.xaxis.set_major_formatter(formatter=StrMethodFormatter(fmt="{x:0.1f}"))
-    ax.set_ylabel(ylabel="y axis label")
+    ax.set_ylabel(ylabel=ylabel)
+    ax.set_title(label=title)
     plt.tight_layout()
-    fig.savefig(fname="fix_ds_histogram.svg")
+    fig.savefig(fname=path_histogram)
 
 
 if __name__ == "__main__":
