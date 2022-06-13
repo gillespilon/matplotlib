28653183e9779ce6fe9662a19e93a39034b64268
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-06-12 20:28:03 -0500

    Create outline of waterfall_chart.py

---
+++ waterfall_chart.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+"""
+docstring
+"""
+
+from pathlib import Path
+import time
+
+import matplotlib.pyplot as plt
+import datasense as ds
+import pandas as pd
+import numpy as np
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
