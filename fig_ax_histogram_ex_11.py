a83abc5b7a88303723a5c9ffabe9f234cd507747
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-10-19 11:47:09 -0500

    Refactor code

--- fig_ax_histogram_ex_11.py
+++ fig_ax_histogram_ex_11.py
@@ -11,16 +11,25 @@ import datasense as ds
 
 
 def main():
-    path_histogram = Path("fig_ax_histogram_ex_11.svg", format="svg")
+    path_histogram = Path(
+        "fig_ax_histogram_ex_11.svg",
+        format="svg"
+    )
     s = ds.random_data(size=300)
     ylabel = "Y axis label"
     xlabel = "X axis label"
     title = "Histogram"
     ds.style_graph()
     fig, ax = ds.plot_histogram(
-        series=s, bin_range=(-4, 4), number_bins=8, bin_label_bool=True
+        series=s,
+        bin_range=(-4, 4),
+        number_bins=8,
+        bin_label_bool=True
     )
-    ax.set_xlabel(xlabel=xlabel, labelpad=30)
+    ax.set_xlabel(
+        xlabel=xlabel,
+        labelpad=30
+    )
     ax.xaxis.set_major_formatter(formatter=StrMethodFormatter(fmt="{x:0.1f}"))
     ax.set_ylabel(ylabel=ylabel)
     ax.set_title(label=title)
