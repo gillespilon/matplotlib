240249a664e1488fa39c5f1d719213f67579aaee
Gilles Pilon <gilles.pilon@gillespilon.ca>
2023-04-09 12:52:03 -0500

    Use ds.plot_boxcox

--- fig_ax_box_cox_ex_09.py
+++ fig_ax_box_cox_ex_09.py
@@ -13,16 +13,13 @@ Reference
 from pathlib import Path
 import time
 
-import matplotlib.pyplot as plt
 from scipy import stats
 import datasense as ds
 
 
 def main():
     start_time = time.perf_counter()
-    colour1, colour2 = "#0077bb", "#33bbee"
     axes_label = "Normal Probability Plot"
-    ylabel1 = "Correlation Coefficient"
     output_url = "box_cox_plot.html"
     xlabel = "Theoretical Quantiles"
     path_box_cox = Path(
@@ -40,7 +37,6 @@ def main():
     header_title = "Box-Cox Plot"
     header_id = "box-cox-plot"
     ylabel2 = "Ordered Values"
-    la, lb = -20, 20
     original_stdout = ds.html_begin(
         output_url=output_url,
         header_title=header_title,
@@ -56,47 +52,8 @@ def main():
     # s = df.iloc[:, 0]
     # comment out next line if reading your own file
     s = stats.loggamma.rvs(5, size=500) + 5
-    # create the Box-Cox normality plot
-    fig, ax = plt.subplots(
-        nrows=1,
-        ncols=1
-    )
-    stats.boxcox_normplot(
-        x=s,
-        la=la,
-        lb=lb,
-        plot=ax
-    )
-    ax.get_lines()[0].set(
-        color=colour1,
-        marker=".",
-        markersize=4
-    )
-    boxcox, lmax_mle, (min_ci, max_ci) = stats.boxcox(
-        x=s,
-        alpha=0.05
-    )
-    ax.axvline(
-        x=min_ci,
-        color=colour2,
-        label=f"min CI = {min_ci:7.3f}"
-    )
-    ax.axvline(
-        lmax_mle,
-        color=colour1,
-        label=f"λ      = {lmax_mle:7.3f}"
-    )
-    ax.axvline(
-        x=max_ci,
-        color=colour2,
-        label=f"max CI = {max_ci:7.3f}"
-    )
-    ax.set_ylabel(ylabel=ylabel1)
-    ax.legend(
-        frameon=False,
-        prop={"family": "monospace", "size": 8}
-    )
-    ds.despine(ax=ax)
+    fig, ax = ds.plot_boxcox(s=s)
+    ax.set_title(label='Box-Cox plot')
     fig.savefig(fname=path_box_cox)
     ds.html_figure(file_name=path_box_cox)
     # create the plot of the untransformed data
@@ -107,6 +64,10 @@ def main():
     fig.savefig(fname=path_box_cox_original)
     ds.html_figure(file_name=path_box_cox_original)
     # create the plot of the transformed data
+    boxcox, lmax_mle, (min_ci, max_ci) = stats.boxcox(
+        x=s,
+        alpha=0.05
+    )
     fig, ax = ds.probability_plot(data=boxcox)
     ax.set_title(label=f"{axes_label}")
     ax.set_xlabel(xlabel=xlabel)
