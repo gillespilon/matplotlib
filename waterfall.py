babb22500e416a4a149e27318a1b8bab768c9273
Gilles Pilon <gilles.pilon@gillespilon.ca>
2024-01-25 14:33:55 -0600

    Add new features, replace values with variables

--- waterfall.py
+++ waterfall.py
@@ -27,15 +27,20 @@ def main():
     # data = {AMOUNT: [350000, -30000, -7500, -25000, 95000, -7000]}
     # df = pd.DataFrame(data=data, index=index)
     PATH_GRAPH_SVG = "waterfall.svg"
-    TITLE = "2014 Sales Waterfall"
+    TITLE = "Sales Waterfall"
     FILE_NAME = "waterfall.xlsx"
-    LAST_BAR_COLOUR = "green"
+    FIRST_BAR_COLOUR = "blue"
+    LAST_BAR_COLOUR = "blue"
     POSITIVE_COLOUR = "green"
     NEGATIVE_COLOUR = "red"
     LAST_COLUMN = "Net"
     GRID_ALPHA = 0.2
+    YLIM_MIN = 260000
+    YLIM_MAX = 400000
     FORMAT = "svg"
     df = ds.read_file(file_name=FILE_NAME)
+    xlabel = df.columns[0]
+    ylabel = df.columns[-1]
     df = df.set_index(df.columns[0])
     amount = df.columns[0]
     df_blank = df[amount].cumsum().shift(1).fillna(0)
@@ -50,23 +55,23 @@ def main():
     x = df.index  # bar positions
     # create the waterfall chart, no need for a stacked argument
     ax.bar(x=x, height=df[amount], width=0.4, bottom=df_blank)
+    ax.set_ylim(YLIM_MIN, YLIM_MAX)
     # set bar colors based on values
     for i, p in enumerate(ax.patches):
         if df.iloc[i][amount] > 0:
             p.set_facecolor(POSITIVE_COLOUR)
         else:
             p.set_facecolor(NEGATIVE_COLOUR)
-    # Change color of the last bar (index -1) to green
+    # Change color of the first and last bars
     # ax.patches contains a list of bar objects in the plot
-    # ax.patches[-1] accesses the last bar in that list
-    # last_bar = ax.patches[-1]
-    # last_bar.set_facecolor(LAST_BAR_COLOUR)  # set desired color here
+    ax.patches[0].set_facecolor(FIRST_BAR_COLOUR)
+    ax.patches[-1].set_facecolor(LAST_BAR_COLOUR)
     ax.plot(step.index, step.values, "b", linewidth=0.5)
     ax.tick_params(axis="x", labelsize=14, labelrotation=90)
     ax.tick_params(axis="y", labelsize=14)
     ax.grid(visible=True, which="major", axis="y", alpha=GRID_ALPHA)
-    ax.set_ylabel(ylabel="ylabel", weight="bold", fontsize=16)
-    ax.set_xlabel(xlabel="xlabel", weight="bold", fontsize=16)
+    ax.set_ylabel(ylabel=ylabel, weight="bold", fontsize=16)
+    ax.set_xlabel(xlabel=xlabel, weight="bold", fontsize=16)
     ax.set_title(label=TITLE, weight="bold", fontsize=18)
     ds.despine(ax=ax)
     fig.savefig(fname=PATH_GRAPH_SVG, format=FORMAT, bbox_inches="tight")
